3ff72d59475b5c20cd773899551262d2f91b5a71
Aaron Kling <webgeek1234@gmail.com>
2017-07-20 13:44:59 -0500

    Fix xhci for t11x

--- drivers/usb/host/xhci-tegra.c
+++ drivers/usb/host/xhci-tegra.c
@@ -2118,7 +2118,8 @@ static void save_ctle_context(struct tegra_xhci_hcd *tegra,
 	tegra->ctle_ctx_saved = (1 << port);
 }
 
-#if defined(CONFIG_ARCH_TEGRA_12x_SOC) || defined(CONFIG_ARCH_TEGRA_13x_SOC)
+#if defined(CONFIG_ARCH_TEGRA_12x_SOC) || defined(CONFIG_ARCH_TEGRA_13x_SOC) || \
+    defined(CONFIG_ARCH_TEGRA_11x_SOC)
 static void tegra_xhci_restore_dfe_context(struct tegra_xhci_hcd *tegra,
 	u8 port)
 {
@@ -2141,7 +2142,9 @@ static void tegra_xhci_restore_dfe_context(struct tegra_xhci_hcd *tegra,
 	padctl_writel(tegra, reg
 			, tegra->padregs->iophy_usb3_padX_ctlY_0[port][3]);
 }
+#endif
 
+#if defined(CONFIG_ARCH_TEGRA_12x_SOC) || defined(CONFIG_ARCH_TEGRA_13x_SOC)
 static void restore_ctle_context(struct tegra_xhci_hcd *tegra,
 	u8 port)
 {
